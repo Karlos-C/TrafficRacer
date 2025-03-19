4032f7d131b13411d1fbb289187c3a600f26278f
Karlos <unit0shift@gmail.com>
2025-03-19 09:44:55 -0400

    #2  Ajout de la détection automatique de la Directional Light

--- Assets/Scripts/DestroyGameObject.cs
+++ Assets/Scripts/DestroyGameObject.cs
@@ -6,12 +6,23 @@ public class DestroyGameObject : MonoBehaviour
     int score = 0;
 
     [SerializeField] TMP_Text txtScore;
+    private Light directionalLight;
+
+    private void Start()
+    {
+        directionalLight = FindObjectOfType<Light>();
+
+        if (directionalLight == null)
+        {
+            Debug.LogError("Aucune Directional Light trouvée dans la scène !");
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Destroy(collision.gameObject); 
-        score++; 
-        txtScore.text = "Score " + score; 
+        Destroy(collision.gameObject);
+        score++;
+        txtScore.text = "Score " + score;
 
         Debug.Log("Score actuel : " + score);
     }
