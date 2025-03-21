5b958f480909ca666b313af028972f60a80be57b
Karlos170 <unit0shift@gmail.com>
2025-03-20 21:25:57 -0400

    #1 Optimisation du spawn rate dynamique

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -9,7 +9,6 @@ public class Spawn : MonoBehaviour
     [SerializeField] float spawnRate = 1.5f;
     int totalCars = 0;
 
-
     void Start()
     {
         StartCoroutine(SpawnCar());
@@ -22,27 +21,12 @@ public class Spawn : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             totalCars++;
 
-            if (totalCars % 5 == 0)
-            {
-                spawnRate -= 0.1f;
-                spawnRate = Mathf.Clamp(spawnRate, 0.3f, 1.5f);
-            }
+            spawnRate *= 0.95f; 
+            spawnRate = Mathf.Clamp(spawnRate, 0.3f, 1.5f); 
 
             int nr = Random.Range(0, cars.Length);
-            int x = Random.Range(0, 3);
-            Instantiate(cars[nr], sp[x].transform.position, transform.rotation);
-        }
-    }
-
-    public void StopSpawn()
-    {
-        StopAllCoroutines();
-
-        GameObject[] cars = GameObject.FindGameObjectsWithTag("Car");
-
-        foreach (GameObject item in cars)
-        {
-            item.GetComponent<MoveForward>().enabled = false;
+            int x = Random.Range(0, sp.Length);
+            Instantiate(cars[nr], sp[x].position, Quaternion.identity);
         }
     }
 }
